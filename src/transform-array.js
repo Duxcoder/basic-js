a073fcf97e651d2fffbeed42f691a42742809721
Duxcoder <iinegve_25@mail.ru>
2023-02-27 20:51:23 +0300

    feat: complete task transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+
+  if (!Array.isArray(arr)) throw new Error(`'arr' parameter must be an instance of the Array!`) 
+  const isDiscard = item => item === '--discard-next' || item === '--discard-prev'
+  const isDouble = item => item === '--double-next' ||  item === '--double-prev'
+
+  const newArr = arr.reduce((createArr, item, i) => {
+    let prevValue = createArr[createArr.length - 1]
+    if (isDouble(prevValue)) {
+      createArr[createArr.length - 1] = prevValue === '--double-next' ? item : createArr.at(-2);
+      createArr.push(item)
+      return createArr
+    }
+    if (isDiscard(item) || isDouble(item)){
+      if (item === arr.at(-1)) return createArr
+    }
+    if (isDiscard(prevValue)) {
+      createArr[createArr.length - 1] = 'delete'
+      if (prevValue === '--discard-next') {
+          createArr.push('delete')
+          return createArr
+      } else {
+        createArr[createArr.length - 2] = 'delete'
+        createArr.push(item)
+        return createArr
+      }
+    }
+    createArr.push(item)
+    return createArr
+  }, [])
+  return newArr.filter(item => item !== 'delete' && item !== undefined)
 }
 
 module.exports = {
